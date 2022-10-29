698f573e8f70f5b5ccb2a9623c751fce8527f4c9
CodeKing2020 <lewistavonga@gmail.com>
2022-10-29 20:43:33 +0200

    basic arithmetic functions in JS added

---
+++ index.js
@@ -0,0 +1,16 @@
+const add = function(a, b) {
+    return a + b
+};
+
+const subtract = function(a, b) {
+    return a - b
+};
+
+const muliply = function(a, b) {
+    return a * b
+};
+
+const divide = function(a, b) {
+    return a / b
+};
+
