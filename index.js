b7f1fe5d74150ca806e0bec1fc832d73087a1b5b
CodeKing2020 <lewistavonga@gmail.com>
2022-10-29 20:48:44 +0200

    operate function added

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ const divide = function(a, b) {
     return a / b
 };
 
+const operate = function(a, b, operation) {
+    switch (operation){
+        case "+":
+            return add(a, b)
+        case "-":
+            return subtract(a, b)
+        case "/":
+            return divide(a, b)
+        case "*":
+            return multiply(a, b)
+        default: 
+            return "ERROR"
+    }
+}
